6c304ea5d4b943a6313657d8af9e0e137ee21bd0
Steve Brownlee <chortlehoort@gmail.com>
2020-07-28 15:27:18 -0500

    Now filtering criminals by officer

--- scripts/officers/OfficerSelect.js
+++ scripts/officers/OfficerSelect.js
@@ -6,18 +6,31 @@ const eventHub = document.querySelector(".container")
 
 // Capture that the user generated a change event by the browser
 eventHub.addEventListener("change", (changeEvent) => {
+    if (changeEvent.target.id === "officerSelect") {
 
+        console.log("OfficerSelect: User chose an officer option")
+
+        const officerSelectedEvent = new CustomEvent("officerSelected", {
+            detail: {
+                officerName: changeEvent.target.value  // "Suzie Police"
+            }
+        })
+
+        console.log("Dispatch custom officerSelected event")
+        eventHub.dispatchEvent(officerSelectedEvent)
+    }
 })
 
 // Render the officer dropdown based on array input
 const render = (officers) => {
+    console.log("OfficerSelect: Officer select rendered to DOM")
     contentTarget.innerHTML = `
         <select class="dropdown" id="officerSelect">
             <option value="0">Please select an officer...</option>
             ${
                 officers.map(
                     officer => {
-                        return `<option value="${ officer.id }">${officer.name}</option>`
+                        return `<option value="${ officer.name }">${officer.name}</option>`
                     }
                 ).join("")
             }
@@ -26,6 +39,7 @@ const render = (officers) => {
 }
 
 export const OfficerSelect = () => {
+    console.log("OfficerSelect: Initial render of officer select")
     getOfficers().then(() => {
         const allOfficers = useOfficers()
         render(allOfficers)
