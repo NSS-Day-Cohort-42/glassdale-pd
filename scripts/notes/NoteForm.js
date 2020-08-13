c7c033d3a9157a85a3495aae4baedb15ffd308f8
Steve Brownlee <chortlehoort@gmail.com>
2020-08-13 09:47:00 -0500

    Detective can now choose a criminal when creating a note

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,4 +1,5 @@
 import { saveNote } from "./NoteProvider.js"
+import { useCriminals, getCriminals } from "../criminals/CriminalProvider.js"
 
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".noteFormContainer")
@@ -10,32 +11,60 @@ eventHub.addEventListener("click", clickEvent => {
         const noteTitle = document.querySelector("#noteForm--title")
         const noteAuthor = document.querySelector("#noteForm--author")
         const noteContent = document.querySelector("#noteForm--content")
+        const noteCriminal = document.querySelector("#noteForm--criminal")
 
-        // Make a new object representation of a note
-        const newNote = {
-            title: noteTitle.value,
-            author: noteAuthor.value,
-            content: noteContent.value,
-            timestamp: Date.now()
+        const criminalId = parseInt(noteCriminal.value)
+
+        // Do not create and save a criminal object if the detective did not choose a criminal
+        if (criminalId !== 0) {
+            // Make a new object representation of a note
+            const newNote = {
+                title: noteTitle.value,
+                author: noteAuthor.value,
+                content: noteContent.value,
+                criminalId: parseInt(noteCriminal.value),
+                timestamp: Date.now()
+            }
+
+            // Change API state and application state
+            saveNote(newNote)
+        }
+        else {
+            window.alert("Please choose a criminal, dummy!")
         }
 
-        // Change API state and application state
-        saveNote(newNote)
     }
 })
 
-const render = () => {
+const render = (criminals) => {
     contentTarget.innerHTML = `
         <section class="noteForm">
             <input type="text" id="noteForm--title" placeholder="Enter note title" />
             <input type="text" id="noteForm--author" placeholder="Your name here" />
+            <select id="noteForm--criminal">
+                <option value="0">Select a criminal...</option>
+                ${
+                    criminals.map(
+                        (criminalObject) => {
+                            return `<option value="${criminalObject.id}">
+                                ${ criminalObject.name }
+                            </option>`
+                        }
+                    )
+                }
+            </select>
             <textarea id="noteForm--content" placeholder="Note text here"></textarea>
 
             <button id="noteForm--saveNote">Save Note</button>
         </section>
     `
+    console.log("Note form rendered")
 }
 
 export const NoteForm = () => {
-    render()
+    getCriminals()
+        .then(() => {
+            const criminals = useCriminals()
+            render(criminals)
+        })
 }
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,13 +1,24 @@
 import { getNotes, useNotes } from "./NoteProvider.js";
 import { NoteHTMLConverter } from "./NoteHTMLCoverter.js";
+import { useCriminals } from "../criminals/CriminalProvider.js";
 
 const contentTarget = document.querySelector(".noteList")
 const eventHub = document.querySelector(".container")
 
-const render = notes => {
-    contentTarget.innerHTML = notes.map(
+const render = (notes) => {
+    const criminals = useCriminals()
+
+    contentTarget.innerHTML = notes.reverse().map(
         (noteObject) => {
-            return NoteHTMLConverter(noteObject)
+            // Find the criminal that this note is about
+            const foundCriminal = criminals.find(
+                (criminalObject) => {
+                    return criminalObject.id === noteObject.criminalId
+                }
+            )
+
+            // Send the criminal to the HTML converter
+            return NoteHTMLConverter(noteObject, foundCriminal)
         }
     ).join("")
 }
